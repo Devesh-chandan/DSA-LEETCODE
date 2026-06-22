494ee77dcc399c72a4094c0322fd7d59dd783ed1
Devesh Chandan <crea8x1@gmail.com>
2026-06-22 21:55:28 +0530

    Time: 2 ms (96.67%), Space: 43.5 MB (54.89%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,32 +1,54 @@
+//MORE OPTIMAL
 class Solution {
-      public boolean canMakeBalloon(int[] freq) {
-        return freq['b' - 'a'] >= 1 &&
-               freq['a' - 'a'] >= 1 &&
-               freq['l' - 'a'] >= 2 &&
-               freq['o' - 'a'] >= 2 &&
-               freq['n' - 'a'] >= 1;
-    }
-
     public int maxNumberOfBalloons(String text) {
-        int count = 0;
         int[] freq = new int[26];
 
-        // Count frequencies
         for (char c : text.toCharArray()) {
             freq[c - 'a']++;
         }
 
-        // Brute-force: form balloons one by one
-        while (canMakeBalloon(freq)) {
-            freq['b' - 'a']--;
-            freq['a' - 'a']--;
-            freq['l' - 'a'] -= 2;
-            freq['o' - 'a'] -= 2;
-            freq['n' - 'a']--;
-
-            count++;
-        }
-
-        return count;
+        return Math.min(
+                Math.min(freq['b' - 'a'], freq['a' - 'a']),
+                Math.min(
+                    Math.min(freq['l' - 'a'] / 2, freq['o' - 'a'] / 2),
+                    freq['n' - 'a']
+                )
+        );
     }
 }
+
+
+//Less Optimal 
+
+// class Solution {
+//       public boolean canMakeBalloon(int[] freq) {
+//         return freq['b' - 'a'] >= 1 &&
+//                freq['a' - 'a'] >= 1 &&
+//                freq['l' - 'a'] >= 2 &&
+//                freq['o' - 'a'] >= 2 &&
+//                freq['n' - 'a'] >= 1;
+//     }
+
+//     public int maxNumberOfBalloons(String text) {
+//         int count = 0;
+//         int[] freq = new int[26];
+
+//         // Count frequencies
+//         for (char c : text.toCharArray()) {
+//             freq[c - 'a']++;
+//         }
+
+//         // Brute-force: form balloons one by one
+//         while (canMakeBalloon(freq)) {
+//             freq['b' - 'a']--;
+//             freq['a' - 'a']--;
+//             freq['l' - 'a'] -= 2;
+//             freq['o' - 'a'] -= 2;
+//             freq['n' - 'a']--;
+
+//             count++;
+//         }
+
+//         return count;
+//     }
+// }
