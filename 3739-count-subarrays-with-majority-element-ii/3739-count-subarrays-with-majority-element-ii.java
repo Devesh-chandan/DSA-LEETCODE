b5309be3e4ee6c002437f52c755a6e17bc129cf6
Devesh Chandan <crea8x1@gmail.com>
2026-06-26 20:39:16 +0530

    Time: 2 ms (100%), Space: 87.9 MB (81.97%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -1,64 +1,25 @@
-import java.util.*;
-
 class Solution {
-
-    static class Fenwick {
-        int[] bit;
-
-        Fenwick(int n) {
-            bit = new int[n + 1];
-        }
-
-        void update(int idx, int val) {
-            while (idx < bit.length) {
-                bit[idx] += val;
-                idx += idx & -idx;
-            }
-        }
-
-        int query(int idx) {
-            int sum = 0;
-            while (idx > 0) {
-                sum += bit[idx];
-                idx -= idx & -idx;
-            }
-            return sum;
-        }
-    }
 
     public long countMajoritySubarrays(int[] nums, int target) {
         int n = nums.length;
-
-        long[] pref = new long[n + 1];
-
-        for (int i = 0; i < n; i++) {
-            pref[i + 1] = pref[i] + (nums[i] == target ? 1 : -1);
-        }
-
-        long[] vals = pref.clone();
-        Arrays.sort(vals);
-
-        Map<Long, Integer> rank = new HashMap<>();
-        int idx = 1;
-
-        for (long v : vals) {
-            if (!rank.containsKey(v)) {
-                rank.put(v, idx++);
-            }
-        }
-
-        Fenwick bit = new Fenwick(idx);
+        // represents the occurrence count of prefix sums -n, -(n-1), ..., 0, 1, ..., n, with index offset by n.
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int cnt = n;
         long ans = 0;
-
-        for (long p : pref) {
-            int r = rank.get(p);
-
-            // count previous prefix sums < current prefix sum
-            ans += bit.query(r - 1);
-
-            bit.update(r, 1);
+        long presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            } else {
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
         }
-
         return ans;
     }
 }
