89cde5df46b425d5049e23f6bb8c28391c2659f1
Devesh Chandan <crea8x1@gmail.com>
2026-07-18 19:15:48 +0530

    Time: 0 ms (100%), Space: 61.5 MB (29.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,27 +1,48 @@
-class Solution {
-    public void rotate(int[] nums, int k) {
+// class Solution {
+//     public void rotate(int[] nums, int k) {
         
-        int n = nums.length;
+//         int n = nums.length;
 
-        // Important when k > n
-       k=k%n;
+//         // Important when k > n
+//        k=k%n;
 
-        int[] result = new int[n];
+//         int[] result = new int[n];
 
-        // Put last k elements at beginning
-        for (int i = n - k; i < n; i++) {
-            result[i - (n - k)] = nums[i];
-        }
+//         // Put last k elements at beginning
+//         for (int i = n - k; i < n; i++) {
+//             result[i - (n - k)] = nums[i];
+//         }
 
-        // Put remaining elements after k positions
-        for (int i = 0; i < n - k; i++) {
-            result[i + k] = nums[i];
-        }
+//         // Put remaining elements after k positions
+//         for (int i = 0; i < n - k; i++) {
+//             result[i + k] = nums[i];
+//         }
 
-        // Copy result back to nums
-        for (int i = 0; i < n; i++) {
-            nums[i] = result[i];
-        }
+//         // Copy result back to nums
+//         for (int i = 0; i < n; i++) {
+//             nums[i] = result[i];
+//         }
+//     }
+// }
+//    Optimal code reversal algo TC=O(N) SC=O(1)
+
+   class Solution {
+    public void rotate(int[] nums, int k) {
+int n=nums.length;
+k=k%n;
+reverse(nums,0,n-1);
+reverse(nums,0,k-1);
+reverse(nums,k,n-1);
     }
-}
-   
+    public void reverse(int[]nums ,int left,int right){
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+             left++;
+        right--;
+        }
+       
+
+    }
+   }
