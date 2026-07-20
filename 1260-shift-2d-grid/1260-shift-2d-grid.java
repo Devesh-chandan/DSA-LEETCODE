f208b6d78b1c35eb1927598809f4e6a5a596b399
Devesh Chandan <crea8x1@gmail.com>
2026-07-20 22:59:35 +0530

    Time: 4 ms (97.01%), Space: 47.4 MB (8.97%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,36 +1,36 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-     
-        int m = grid.length;
-        int n = grid[0].length;
-        int size=m*n;
+        int n=grid.length;
+        int m=grid[0].length;
+        int size=n*m;
         int temp[]=new int[size];
         int idn=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 temp[idn++]=grid[i][j];
 
             }
         }
-
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>>res=new ArrayList<>();
         idn=0;
-        for(int i=0;i<m;i++){
+        for(int i=0;i<n;i++){
             List<Integer> list=new ArrayList<>();
-
-            for(int j=0;j<n;j++){
-                k = k % size;
-                 int idx = idn - k;
-
-        if (idx < 0) {
-            idx += size;
-        }
+            
+            for(int j=0;j<m;j++){
+                k=k%size;
+                int idx=idn-k;
+                if(idx<0){
+                    idx+=size;
+                }
                 int val=temp[idx];
                 list.add(val);
                 idn++;
+
             }
             res.add(list);
         }
+        
         return res;
+        
     }
 }
