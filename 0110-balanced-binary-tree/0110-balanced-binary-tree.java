68959a890b9f5a629c4bc0ec4c7d5246030587cd
Devesh Chandan <crea8x1@gmail.com>
2026-07-21 17:20:14 +0530

    Time: 0 ms (100%), Space: 45.7 MB (23.34%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -13,32 +13,69 @@
  *     }
  * }
  */
+
+
+//  TC is O(N^2) not optimized because leftdepth and rightdepth is called two times in two functions
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//        if(root==null){
+//             return true;
+
+//         }
+//         int leftdepth=depth(root.left);
+//         int rightdepth=depth(root.right);
+//         int diff=Math.abs(leftdepth-rightdepth);
+//         if(diff>1){
+//             return false;
+//         }
+//         return isBalanced(root.left)&&isBalanced(root.right);
+        
+        
+//     }
+//     public int depth(TreeNode root){
+//         if(root==null){
+//             return 0;
+//         }
+
+//         int leftmax=depth(root.left);
+//         int rightmax=depth(root.right);
+//         return 1+Math.max(leftmax,rightmax);
+//     }
+    
+    
+// }
+
+
+// Optimal solution TC = O(n) 
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
-       if(root==null){
-            return true;
-
-        }
-        int leftdepth=depth(root.left);
-        int rightdepth=depth(root.right);
-        int diff=Math.abs(leftdepth-rightdepth);
-        if(diff>1){
-            return false;
-        }
-        return isBalanced(root.left)&&isBalanced(root.right);
-        
-        
+        return depth(root)!=-1;
     }
+        
+        
+        
+    
     public int depth(TreeNode root){
         if(root==null){
             return 0;
         }
 
         int leftmax=depth(root.left);
+        if (leftmax == -1) {
+            return -1;
+        }
         int rightmax=depth(root.right);
-        return 1+Math.max(leftmax,rightmax);
+        if (rightmax == -1) {
+            return -1;
+        }
+         if (Math.abs(leftmax - rightmax) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(leftmax, rightmax);
     }
     
     
 }
-
+    
