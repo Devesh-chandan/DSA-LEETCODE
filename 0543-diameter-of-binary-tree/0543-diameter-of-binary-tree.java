1e0f6c248c24519d04cc9f5741bf11d386372d24
Devesh Chandan <crea8x1@gmail.com>
2026-07-21 21:25:52 +0530

    Time: 1 ms (46.47%), Space: 46.9 MB (73.92%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,30 +13,53 @@
  *     }
  * }
  */
+
+//  Brute force approach not optimal 
+// class Solution {
+//     public int diameterOfBinaryTree(TreeNode root) {
+
+//         if (root == null) {
+//             return 0;
+//         }
+//         int lh = ht(root.left);
+//         int rh = ht(root.right);
+
+//         int leftdia = diameterOfBinaryTree(root.left);
+//         int rightdia = diameterOfBinaryTree(root.right);
+//         return Math.max(lh + rh, Math.max(leftdia, rightdia));
+
+//     }
+
+//     public int ht(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+//         int leftht = ht(root.left);
+//         int rightht = ht(root.right);
+//         return 1 + Math.max(leftht, rightht);
+//     }
+// }
+
+// Optimal approach O(N)
 class Solution {
+
+    int maxi = 0;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        
-        
-        if(root==null){
-            return 0;
-        }
-        int lh=ht(root.left);
-       int rh=ht(root.right);
-      
-        int leftdia=diameterOfBinaryTree(root.left);
-        int rightdia=diameterOfBinaryTree(root.right);
-        return Math.max(lh + rh, Math.max(leftdia, rightdia));
-
-
-
-        
+        ht(root);
+        return maxi;
     }
-    public int ht(TreeNode root){
-        if(root==null){
+
+    public int ht(TreeNode root) {
+        if (root == null) {
             return 0;
         }
-        int leftht=ht(root.left);
-        int rightht=ht(root.right);
-    return 1+Math.max(leftht,rightht);
+
+        int lh = ht(root.left);
+        int rh = ht(root.right);
+
+        maxi = Math.max(maxi, lh + rh);
+
+        return 1 + Math.max(lh, rh);
     }
 }
