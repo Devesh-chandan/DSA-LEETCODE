aab162f0e2fc24ff9c771a7ddb3b66bf226dc391
Devesh Chandan <crea8x1@gmail.com>
2026-07-24 17:19:26 +0530

    Time: 0 ms (100%), Space: 43.9 MB (46.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,64 @@
+// TC=O((m+n)log(m+n))
+// SC=O(m+n)
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         // int[] ans=sorted(nums1,m,nums2,n);
+//         // for (int i = 0; i < m + n; i++) {
+//         //     nums1[i] = ans[i];
+//         // }
+
+//     }
+//     public int[] sorted(int[] nums1,int m,int[]nums2,int n){
+//         int[] merged=new int[m+n];
+//         ArrayList<Integer> nums=new ArrayList<>();
+//         for(int i=0;i<m;i++){
+//             nums.add(nums1[i]);
+//         }
+//         for(int j=0;j<n;j++){
+//             nums.add(nums2[j]);
+//         }
+//         System.out.println(nums);
+//         Collections.sort(nums);
+//         System.out.println(nums);
+//         for(int k=0;k<nums.size();k++){
+//             merged[k]=nums.get(k);
+//         }
+//         return merged;
+
+//     }
+// }
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] ans=sorted(nums1,m,nums2,n);
+        int ans[] = sorted(nums1, m, nums2, n);
         for (int i = 0; i < m + n; i++) {
             nums1[i] = ans[i];
         }
-        
-        
-    }
-    public int[] sorted(int[] nums1,int m,int[]nums2,int n){
-        int[] merged=new int[m+n];
-        ArrayList<Integer> nums=new ArrayList<>();
-        for(int i=0;i<m;i++){
-            nums.add(nums1[i]);
-        }
-        for(int j=0;j<n;j++){
-            nums.add(nums2[j]);
-        }
-        System.out.println(nums);
-        Collections.sort(nums);
-        System.out.println(nums);
-        for(int k=0;k<nums.size();k++){
-            merged[k]=nums.get(k);
-        }
-        return merged;
 
     }
+
+    public int[] sorted(int[] nums1, int m, int[] nums2, int n){
+        int[] sorted=new int[m+n];
+        int i = 0;
+int j = 0;
+int k = 0;
+
+while(i < m && j < n){
+
+    if(nums1[i] <= nums2[j]){
+        sorted[k++] = nums1[i++];
+    }else{
+        sorted[k++] = nums2[j++];
+    }
+}
+
+while(i < m)
+    sorted[k++] = nums1[i++];
+
+while(j < n)
+    sorted[k++] = nums2[j++];
+        return sorted;
+    }
+
 }
