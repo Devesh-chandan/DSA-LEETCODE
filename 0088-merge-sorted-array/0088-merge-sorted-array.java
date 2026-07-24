6439c1bd94a1ef55e5703b07c470b196d4439de3
Devesh Chandan <crea8x1@gmail.com>
2026-07-24 17:28:41 +0530

    Time: 0 ms (100%), Space: 44 MB (16.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -29,36 +29,67 @@
 //     }
 // }
 
+
+
+// Approach 2 O(m+n)
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int ans[] = sorted(nums1, m, nums2, n);
+//         for (int i = 0; i < m + n; i++) {
+//             nums1[i] = ans[i];
+//         }
+
+//     }
+
+//     public int[] sorted(int[] nums1, int m, int[] nums2, int n) {
+//         int[] sorted = new int[m + n];
+//         int i = 0;
+//         int j = 0;
+//         int k = 0;
+
+//         while (i < m && j < n) {
+
+//             if (nums1[i] <= nums2[j]) {
+//                 sorted[k++] = nums1[i++];
+//             } else {
+//                 sorted[k++] = nums2[j++];
+//             }
+//         }
+
+//         while (i < m)
+//             sorted[k++] = nums1[i++];
+
+//         while (j < n)
+//             sorted[k++] = nums2[j++];
+//         return sorted;
+//     }
+
+// }
+
+
+// Approach 3 O()
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int ans[] = sorted(nums1, m, nums2, n);
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = ans[i];
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+              i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+ k--;
+
+            }
+           
+            while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
-
-    }
-
-    public int[] sorted(int[] nums1, int m, int[] nums2, int n){
-        int[] sorted=new int[m+n];
-        int i = 0;
-int j = 0;
-int k = 0;
-
-while(i < m && j < n){
-
-    if(nums1[i] <= nums2[j]){
-        sorted[k++] = nums1[i++];
-    }else{
-        sorted[k++] = nums2[j++];
-    }
-}
-
-while(i < m)
-    sorted[k++] = nums1[i++];
-
-while(j < n)
-    sorted[k++] = nums2[j++];
-        return sorted;
-    }
-
+        }
 }
