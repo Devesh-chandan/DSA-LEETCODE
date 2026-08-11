986e2c2aacbf2111516179c45666b60d69fd5a24
Devesh Chandan <crea8x1@gmail.com>
2026-08-11 22:25:45 +0530

    Time: 42 ms (5.22%), Space: 83.5 MB (5.05%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private boolean getPath1(TreeNode root,ArrayList<Integer>arr1,TreeNode p){
+        if(root==null){
+            return false;
+        }
+        arr1.add(root.val);
+        if(root.val==p.val){
+            return true;
+        }
+        if(getPath1(root.left,arr1,p) || getPath1(root.right,arr1,p)){
+            return true;
+        }
+        arr1.remove(arr1.size()-1);
+        return false;
+
+    }
+    private boolean getPath2(TreeNode root,ArrayList<Integer>arr2,TreeNode q){
+         if(root==null){
+            return false;
+        }
+        arr2.add(root.val);
+        if(root.val==q.val){
+            return true;
+        }
+        if(getPath2(root.left,arr2,q) || getPath2(root.right,arr2,q)){
+            return true;
+        }
+        arr2.remove(arr2.size()-1);
+        return false;
+
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ans=root;
+        ArrayList<Integer> arr1=new ArrayList<>();
+        ArrayList<Integer> arr2=new ArrayList<>();
+        if(root==null){
+            return ans;
+            
+        }
+        getPath1(root,arr1,p);
+        getPath2(root,arr2,q);
+        int idx1=1;
+        int idx2=1;
+        while(idx1<arr1.size() && idx2<arr2.size()){
+            if(arr1.get(idx1).equals(arr2.get(idx2))){
+                ans=new TreeNode(arr1.get(idx1));
+            }
+            idx1++;
+            idx2++;
+        }
+return ans;
+        
+    }
+}
