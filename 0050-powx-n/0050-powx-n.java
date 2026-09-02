7c5e48fd5b80cd7905844e3110d321cf7a6ab39a
Devesh Chandan <crea8x1@gmail.com>
2026-09-02 15:20:56 +0530

    Time: 0 ms (100%), Space: 47.9 MB (64.49%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -6,17 +6,17 @@ class Solution {
             N=-N;
         }
         return fastpow(x,N);
-
         
     }
     public double fastpow(double x,long n){
-        if (n == 0) return 1;
-        double half=fastpow(x, n/2);
-        if(n%2==0){
-            return half*half;
-        }else{
-            return half*half*x;
+        if(n==0){
+            return 1;
         }
-
+double half=fastpow(x,n/2);
+if(n%2==0){
+    return half*half;
+}else{
+    return half*half*x;
+}
     }
 }
