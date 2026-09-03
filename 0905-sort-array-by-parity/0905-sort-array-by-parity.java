1bcec33d77a09f4f5d1967ffeec1dcc424c315a6
Devesh Chandan <crea8x1@gmail.com>
2026-09-03 18:29:45 +0530

    Time: 1 ms (45.92%), Space: 46.7 MB (84.96%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,22 +1,51 @@
+// NOT OPTIMIZED SC O(1)   TC O(N)
+
+
+// class Solution {
+//     public int[] sortArrayByParity(int[] nums) {
+//         int ans[]=new int[nums.length];
+//         ArrayList<Integer> res=new ArrayList<>();
+//         for(int i=0;i<nums.length;i++){
+// if(nums[i]%2==0){
+//    res.add(nums[i]);
+// }
+//         }
+//         for(int i=0;i<nums.length;i++){
+// if(nums[i]%2!=0){
+//    res.add(nums[i]);
+// }
+//         }
+// for(int i=0;i<res.size();i++){
+// ans[i]=res.get(i);
+// }
+
+// return ans;
+        
+//     }
+// }
+
+// OPTIMIZED SC O(1)   TC O(N)
+
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int ans[]=new int[nums.length];
-        ArrayList<Integer> res=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-if(nums[i]%2==0){
-   res.add(nums[i]);
-}
-        }
-        for(int i=0;i<nums.length;i++){
-if(nums[i]%2!=0){
-   res.add(nums[i]);
-}
-        }
-for(int i=0;i<res.size();i++){
-ans[i]=res.get(i);
-}
+      
+        int left=0;
+        int right=nums.length-1;
+        while(left<right){
+            if(left<right && nums[left]%2==0){
+                left++;
+            }
+            if(left<right && nums[right]%2!=0){
+                right--;
+            }
 
-return ans;
-        
+
+            if(left<right){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+            }
+        }
+        return nums;
     }
 }
