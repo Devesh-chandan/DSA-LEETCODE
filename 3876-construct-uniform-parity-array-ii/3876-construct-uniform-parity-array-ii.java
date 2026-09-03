4549fe581336d0ace78390dcd8885eaef7789be7
Devesh Chandan <crea8x1@gmail.com>
2026-09-03 10:19:32 +0530

    Time: 12 ms (22.46%), Space: 121.6 MB (62.32%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,35 +1,31 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int minodd=Integer.MAX_VALUE;
-        int mineven=Integer.MAX_VALUE;
-        int even=0;
-        int odd=0;
+        int minodd = Integer.MAX_VALUE;
+        int mineven = Integer.MAX_VALUE;
+        int even = 0;
+        int odd = 0;
 
-        for(int i=0;i<nums1.length;i++){
-            if(nums1[i]%2==0){
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] % 2 == 0) {
                 even++;
-            }
-            else{
+            } else {
                 odd++;
 
             }
-            if(nums1[i]<minodd && nums1[i]%2!=0){
-                minodd=nums1[i];
-            }else if(nums1[i]<mineven && nums1[i]%2==0){
-                mineven=nums1[i];
+            if (nums1[i] < minodd && nums1[i] % 2 != 0) {
+                minodd = nums1[i];
+            } else if (nums1[i] < mineven && nums1[i] % 2 == 0) {
+                mineven = nums1[i];
             }
         }
-        if(even==0 || odd==0){
+        if (even == 0 || odd == 0) {
             return true;
         }
-        
 
-        
         System.out.println(minodd);
         System.out.println(mineven);
 
-        
-        if(mineven>minodd){
+        if (mineven > minodd) {
             return true;
         }
         return false;
