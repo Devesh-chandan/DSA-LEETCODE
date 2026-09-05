a3f834abce1410b67a9f0bbcacf93b6c7d8f0619
Devesh Chandan <crea8x1@gmail.com>
2026-09-05 14:02:35 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (70.95%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,20 +1,45 @@
+// class Solution {
+//     public int firstStableIndex(int[] nums, int k) {
+//         int n = nums.length;
+//         for (int i = 0; i < n; i++) {
+//             int currmax = nums[0];
+//             for (int j = 0; j <= i; j++) {
+//                 currmax = Math.max(currmax, nums[j]);
+//             }
+//             int currmin = nums[i];
+//             for (int j = i; j < n; j++) {
+//                 currmin = Math.min(currmin, nums[j]);
+//             }
+//             if (currmax - currmin <= k) {
+//                 return i;
+//             }
+
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int currmax = nums[0];
-            for (int j = 0; j <= i; j++) {
-                currmax = Math.max(currmax, nums[j]);
-            }
-            int currmin = nums[i];
-            for (int j = i; j < n; j++) {
-                currmin = Math.min(currmin, nums[j]);
-            }
-            if (currmax - currmin <= k) {
+        int[] suffixmin=new int[nums.length];
+        int[] prefixmax=new int[nums.length];
+        suffixmin[nums.length-1]=nums[nums.length-1];
+       
+        prefixmax[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefixmax[i]=Math.max(nums[i],prefixmax[i-1]);
+        }
+        for(int i=nums.length-2;i>=0;i--){
+            suffixmin[i]=Math.min(suffixmin[i+1],nums[i]);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(prefixmax[i]-suffixmin[i] <=k){
                 return i;
             }
-
         }
+
         return -1;
     }
 }
