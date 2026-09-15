fc838ba37cb0ceae4a6641628caad14a3906245f
Devesh Chandan <crea8x1@gmail.com>
2026-09-15 12:38:46 +0530

    Time: 119 ms (28.14%), Space: 61.9 MB (40.7%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,24 +1,57 @@
+// class Solution {
+//     public int maxPalindromes(String s, int k) {
+//         int n = s.length(), ans = 0, end = -1;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int l0 : new int[]{i - 1, i}) {
+//                 int l = l0, r = i;
+
+//                 while(l>=0 && r < n && s.charAt(l) == s.charAt(r)){
+//                     if (r - l + 1 >= k && l > end) {
+//                         ans++;
+//                         end = r;
+//                         break;
+//                     }
+
+//                     l--;
+//                     r++;
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
+
+//approach 2
 class Solution {
     public int maxPalindromes(String s, int k) {
-        int n = s.length(), ans = 0, end = -1;
+        int n = s.length();
+        boolean[][] palindrome = new boolean[n][n];
+        int[] dp = new int[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            for (int l0 : new int[]{i - 1, i}) {
-                int l = l0, r = i;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) &&
+                    (j - i < 2 || palindrome[i + 1][j - 1])) {
+                    palindrome[i][j] = true;
+                }
+            }
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1];
 
-                while(l>=0 && r < n && s.charAt(l) == s.charAt(r)){
-                    if (r - l + 1 >= k && l > end) {
-                        ans++;
-                        end = r;
-                        break;
-                    }
-
-                    l--;
-                    r++;
+            for (int j = 0; j < i; j++) {
+                if (i - j >= k && palindrome[j][i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
 
-        return ans;
+        return dp[n];
     }
 }
